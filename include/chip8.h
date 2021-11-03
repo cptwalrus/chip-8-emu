c1ecb06d216f8db23042129eb13f902ae9a0a3ae
cptwalrus <cptwalrus@cptwalrus.com>
2021-11-02 23:27:17 -0400

    There is now a white square in the corner... need to figure out scaling or something...

--- include/chip8.h
+++ include/chip8.h
@@ -9,6 +9,11 @@ class Chip8
         Chip8();
 
         void emulateCycle();
+        
+        //graphics - Chip8 is just black and white/on off pixels
+        //  NOTE: Might be easier to implement a 2d array for this... will see when actually doing gfx
+        //  TEMP move to public as I test stuff and deside how I want this engineered
+        std::array<unsigned char, 64*32*4> gfx;
 
     private:
 
@@ -22,10 +27,6 @@ class Chip8
 
         //15 regsisters - 16th is carry register
         std::array<unsigned char, 16> V;
-
-        //graphics - Chip8 is just black and white/on off pixels
-        //  NOTE: Might be easier to implement a 2d array for this... will see when actually doing gfx
-        std::array<unsigned char, 64*32> gfx;
 
         std::array<unsigned char, 80> fontset;
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,27 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(64, 32), "Chip-8 Emu");
 
+    Chip8 chip8;
+
+    sf::Texture texture;
+    texture.create(64, 32);
+
+    sf::Sprite sprite(texture);
+
     while(window.isOpen())
     {
+        for(int pixel = 0; pixel < chip8.gfx.size(); pixel+=4)
+        {
+          chip8.gfx[pixel] = 255;
+          chip8.gfx[pixel+1] = 255;
+          chip8.gfx[pixel+2] = 255;
+          chip8.gfx[pixel+3] = 255;
+            
+        }
+
+        texture.update(chip8.gfx.data());
+
+        window.draw(sprite);
 
         sf::Event event;
         while(window.pollEvent(event))
@@ -19,8 +38,7 @@ int main()
             }
         }
 
-        window.clear(sf::Color::Black);
-
+        //window.clear(sf::Color::Black);
         window.display();
     }
     return 0;
